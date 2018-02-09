155fa00160d1236812f9ace54b4bfa5a2cff58a8
Elliott Bussell <ehb31@cam.ac.uk>
2018-02-09 16:27:22 +0000

    Removed old test code

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from .simulator import main, run_epidemics, Simulator, __version__
 from . import utilities
-from . import test
 
-__all__ = ["main", "run_epidemics", "Simulator", "__version__", "utilities", "test"]
+__all__ = ["main", "run_epidemics", "Simulator", "__version__", "utilities"]
